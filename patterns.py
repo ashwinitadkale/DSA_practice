5b9c711924c4ac1d0209654ab2cdbd283cafcb3f
Ashwini Tadkale <ashwinitadkale@gmail.com>
2025-10-08 21:59:59 +0530

    updated the file

--- patterns.py
+++ patterns.py
@@ -8,24 +8,23 @@
 # for i in range(n):
 #     for j in range(n):
 #         print('*',end=' ')
-# #     print()
-# ****
-# ***
-# **
-# *
-n=int(input())
-for i in range(n,1):
-    for j in range(i):
-        print('*',end=' ')
-    print()
+#     print()
+
+# * 
+# * *
+# * * *
+# * * * *
+# * * * * *
 
 # n=int(input())
 # for i in range(n):
 #     for j in range(i+1):
 #         print('*',end=' ')
 #     print()
-# n=int(input())
-# for i in range(n):
-#     for j in range(1,i+1):
-#         print(j,end=' ')
-#     print()
+
+
+n=int(input())
+for i in range(n):
+    for j in range(1,i+1):
+        print(j,end=' ')
+    print()
--- patterns2.py
+++ patterns2.py
@@ -2,8 +2,8 @@
 # ***
 # **
 # *
-n=int(9)
-for i in range(n,1):
+n=4
+for i in range(n,0,-1):
     for j in range(i):
         print("*",end=' ')
     print()
