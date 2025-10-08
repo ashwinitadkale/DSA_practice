02945f3fa5f87c2feace98628ebe06725558459a
Ashwini Tadkale <ashwinitadkale@gmail.com>
2025-10-08 23:37:48 +0530

    added on pattern

--- patterns2.py
+++ patterns2.py
@@ -2,8 +2,20 @@
 # ***
 # **
 # *
-n=4
+# n=4
+# for i in range(n,0,-1):
+#     for j in range(i):
+#         print("*",end=' ')
+#     print()
+
+# 1 2 3 4 5 6
+# 1 2 3 4 5
+# 1 2 3 4
+# 1 2 3
+# 1 2 
+# 1
+n=6
 for i in range(n,0,-1):
-    for j in range(i):
-        print("*",end=' ')
+    for j in range(i,0,-1):
+        print(n-j+1,end=' ')
     print()
