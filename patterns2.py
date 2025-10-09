597f90ecf28df32a897fe922770812b574a95f81
Ashwini Tadkale <ashwinitadkale@gmail.com>
2025-10-09 23:46:01 +0530

    add pattern 7

--- patterns2.py
+++ patterns2.py
@@ -26,14 +26,27 @@ for i in range(n,0,-1):
 #   *******  
 #  ********* 
 # ***********
+# n = int(input("Enter number of rows: "))
+# for i in range(n):
+#     # print spaces
+#     for j in range(n - i - 1):
+#         print(" ", end="")
+    
+#     # print stars
+#     for j in range(2 * i + 1):
+#         print("*", end="")
+    
+#     # move to next line
+#     print()
+
 n = int(input("Enter number of rows: "))
-for i in range(n):
+for i in range(n,0,-1):
     # print spaces
-    for j in range(n - i - 1):
+    for j in range(n - i):
         print(" ", end="")
     
     # print stars
-    for j in range(2 * i + 1):
+    for j in range(2 * i -1):
         print("*", end="")
     
     # move to next line
