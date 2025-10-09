1ee04a47ceff740492adf2da02cb24b876afce55
Ashwini Tadkale <ashwinitadkale@gmail.com>
2025-10-09 23:37:32 +0530

    added start patter

--- patterns2.py
+++ patterns2.py
@@ -2,11 +2,11 @@
 # ***
 # **
 # *
-# n=4
-# for i in range(n,0,-1):
-#     for j in range(i):
-#         print("*",end=' ')
-#     print()
+n=4
+for i in range(n,0,-1):
+    for j in range(i):
+        print("*",end=' ')
+    print()
 
 # 1 2 3 4 5 6
 # 1 2 3 4 5
@@ -14,8 +14,27 @@
 # 1 2 3
 # 1 2 
 # 1
-n=6
-for i in range(n,0,-1):
-    for j in range(i,0,-1):
-        print(n-j+1,end=' ')
+# n=6
+# for i in range(n,0,-1):
+#     for j in range(i,0,-1):
+#         print(n-j+1,end=' ')
+#     print()
+
+#      *     
+#     ***    
+#    *****   
+#   *******  
+#  ********* 
+# ***********
+n = int(input("Enter number of rows: "))
+for i in range(n):
+    # print spaces
+    for j in range(n - i - 1):
+        print(" ", end="")
+    
+    # print stars
+    for j in range(2 * i + 1):
+        print("*", end="")
+    
+    # move to next line
     print()
