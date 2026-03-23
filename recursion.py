f688887402194bef9388b6158726aa9262eb71d3
Ashwini Tadkale <ashwinitadkale@gmail.com>
2026-03-23 21:46:10 +0530

    updated with sum of first n numbers code

--- recursion.py
+++ recursion.py
@@ -1,8 +1,8 @@
-# def print_name(n):
-#     if n == 0:   # Base case
-#         return
-#     print("Ashwini") 
-#     print_name(n-1)    # Recursive call
+def print_name(n):
+    if n == 0:   # Base case
+        return
+    print("Ashwini") 
+    print_name(n-1)    # Recursive call
 
 
 #Print 1 to N using recursion
@@ -14,15 +14,15 @@ def print_num(n):
 
 #sum of first N numbers
 def sum_n(n):
-    if n==0:
-        return
+    if n==1:
+        return 1
     return n+sum_n(n-1)
 
 
 #input
 N = 5
-print(sum_n)
-# print_num(N)
-# print_name(N)
+print("sum of first N integers is:",sum_n(N))
+print("print 1 to N numbers:",print_num(N))
+print("print name N times",print_name(N))
 
 
